25996d18b9f3180a46901c27da7429d91c795d63
valentinLampriere <valentin.lampriere1@gmail.com>
2021-01-20 10:28:50 +0100

    Initialize emgucv

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+public class Manager : MonoBehaviour {
+    void Start() {
+
+    }
+}
